9f248c1221e7e59c787dbf5927c534ad50b0695f
FadhilJemAR <fadhilar91128@gmail.com>
2026-09-07 13:56:19 +0800

    Update service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = "pwa-lab-v4";
 
 const ASSETS = [
-  "index.html",
-  "script.js"
+    "index.html",
+    "script.js"
 ];
 
 console.log("Service Worker: Didaftarkan");
 
-self.addEventListener("install",(event)=>{
+self.addEventListener("install", (event) => {
     console.log("Service Worker Diinstall")
     self.skipWaiting();
     //Masukkan semua aset ke cache 
@@ -17,48 +17,32 @@ self.addEventListener("install",(event)=>{
                 console.log("Cache Baru dibuat: " + CACHE_NAME);
                 return cache.addAll(ASSETS);
             })
-    );  
+    );
 });
 
-self.addEventListener("activate",(event)=>{
+self.addEventListener("activate", (event) => {
     console.log("Mengaktifkan Service Worker Baru");
-     //Bersihkan cache lama
-     event.waitUntil(
-         caches.keys().then((cacheNames) => {
-             return Promise.all(
-                 cacheNames.map((cacheName) => {
-                     if (cacheName !== CACHE_NAME) {
-                         console.log("Cache lama dihapus: " + cacheName);
-                         return caches.delete(cacheName);
-                     }
-                 })
-             );
-         })
-     );
-     event.waitUntil(self.clients.claim())    
+    //Bersihkan cache lama
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("Cache lama dihapus: " + cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    event.waitUntil(self.clients.claim())
 });
 
-self.addEventListener("fetch",(event)=>{
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        console.log("Cache ditemukan ! :", response);
-
-        return response;
-      }
-      console.log("Tidak ada cache, memuat dari jaringan...");
-
-      return fetch(event.request)
-        .then((response) => {
-          console.log("Response dari jaringan:", response);
-
-          return response;
-        })
-        .catch((error) => {
-          console.error("Fetching failed:", error);
-
-          throw error;
-        });
-    }),
-  );
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+     );
+    
 });
